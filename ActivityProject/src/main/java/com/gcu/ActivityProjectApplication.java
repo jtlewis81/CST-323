96049315ead295396463d73aeaee8d5707318cf7
jtlewis81 <jtlewis91481@gmail.com>
2022-12-17 01:31:07 -0500

    final update fix

---
+++ ActivityProject/src/main/java/com/gcu/ActivityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityProjectApplication.class, args);
+	}
+
+}
